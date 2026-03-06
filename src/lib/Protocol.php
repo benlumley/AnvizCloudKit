098f81bc0a6fb08d7738376697e994697677d6de
Ben Lumley <ben@benlumley.co.uk>
2026-03-06 10:11:50 +0000

    Set all nexttime values to 10 (gSOAP max)
    Standardise register, error, forbidden, and nocommand responses
    to use the maximum allowed heartbeat interval.

--- src/lib/Protocol.php
+++ src/lib/Protocol.php
@@ -943,22 +943,22 @@ class Protocol
 
     public static function showRegister($device_id)
     {
-        return Tools::R(self::joinCommand('11111111', $device_id, '11111111', AnvizConstants::CMD_REGESTER, 1, ''));
+        return Tools::R(self::joinCommand('11111111', $device_id, '11111111', AnvizConstants::CMD_REGESTER, 10, ''));
     }
 
     public static function showError($sha1, $device_uuid, $command = '')
     {
-        return Tools::R(self::joinCommand($sha1, $device_uuid, '11111111', AnvizConstants::CMD_ERROR, 5, $command));
+        return Tools::R(self::joinCommand($sha1, $device_uuid, '11111111', AnvizConstants::CMD_ERROR, 10, $command));
     }
 
     public static function showForbidden()
     {
-        return Tools::R(self::joinCommand('11111111', '22222222', '22222222', AnvizConstants::CMD_FORBIDDEN, 5, ''));
+        return Tools::R(self::joinCommand('11111111', '22222222', '22222222', AnvizConstants::CMD_FORBIDDEN, 10, ''));
     }
 
     public static function showNocommand($token, $device_id)
     {
-        return Protocol::joinCommand($token, $device_id, '11111111', AnvizConstants::CMD_NOCOMMAND, 5);
+        return Protocol::joinCommand($token, $device_id, '11111111', AnvizConstants::CMD_NOCOMMAND, 10);
     }
 
     /**
